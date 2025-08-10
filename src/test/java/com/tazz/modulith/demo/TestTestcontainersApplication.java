0b15bc835a39d6c514a58ec9b0c2b9cf3fb181da
Tazzmanian <banskaliqtabg@yahoo.com>
2025-08-10 23:08:16 +0300

    add profiles

---
+++ src/test/java/com/tazz/modulith/demo/TestTestcontainersApplication.java
@@ -0,0 +1,31 @@
+package com.tazz.modulith.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@Profile("testcontainers")
+@TestConfiguration(proxyBeanMethods = false)
+public class TestTestcontainersApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+//    @Bean
+//    @ServiceConnection
+//    DockerComposeContainer<?> pgAdmin() {
+//        return new DockerComposeContainer<>("dpage/pgadmin4");
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(ModulithDemoApplication::main)
+                .with(TestTestcontainersApplication.class).run(args);
+    }
+}
