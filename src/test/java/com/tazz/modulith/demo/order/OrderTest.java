0273544bd8a1d8414c098085c506e1d6da60f89c
Tazzmanian <banskaliqtabg@yahoo.com>
2025-08-15 16:17:45 +0300

    Add publisher test

--- src/test/java/com/tazz/modulith/demo/order/OrderTest.java
+++ src/test/java/com/tazz/modulith/demo/order/OrderTest.java
@@ -3,15 +3,18 @@ package com.tazz.modulith.demo.order;
 import com.tazz.modulith.demo.TestTestcontainerConfig;
 import com.tazz.modulith.demo.order.dto.InventoryRequestDto;
 import com.tazz.modulith.demo.order.dto.OrderDto;
+import com.tazz.modulith.demo.order.dto.OrderPaymentDto;
 import com.tazz.modulith.demo.order.dto.OrderResponseDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.modulith.test.ApplicationModuleTest;
+import org.springframework.modulith.test.Scenario;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
+import java.util.UUID;
 
 @Import(TestTestcontainerConfig.class)
 @ActiveProfiles("testcontainers")
@@ -36,5 +39,13 @@ class  OrderTest {
 
         Assertions.assertEquals("Order under process", responseDto.message());
         Assertions.assertEquals(102, responseDto.statusCode());
+    }
+
+    @Test
+    void publishOrderPayment(Scenario scenario) {
+        scenario.publish(new OrderPaymentDto(UUID.randomUUID().toString(), 5000))
+                .andWaitForEventOfType(OrderPaymentDto.class)
+                .matching(event -> event.amount() == 5000)
+                .toArriveAndVerify(ev -> System.out.println(ev.amount()));
     }
 }
