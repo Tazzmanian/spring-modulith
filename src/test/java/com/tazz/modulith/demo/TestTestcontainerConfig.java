98285883da653ffa29afed1fd3b8a4dac58ab50a
Tazzmanian <banskaliqtabg@yahoo.com>
2025-08-15 16:06:44 +0300

    Add integration test, Separate the testcontainerconfig for local development and tests

--- src/test/java/com/tazz/modulith/demo/TestTestcontainerConfig.java
+++ src/test/java/com/tazz/modulith/demo/TestTestcontainerConfig.java
@@ -1,6 +1,5 @@
 package com.tazz.modulith.demo;
 
-import com.github.dockerjava.api.model.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -14,7 +13,7 @@ import org.testcontainers.utility.DockerImageName;
 
 // https://spring.io/blog/2023/06/23/improved-testcontainers-support-in-spring-boot-3-1
 @TestConfiguration(proxyBeanMethods = false)
-public class TestcontainerConfig {
+public class TestTestcontainerConfig {
 
     @Bean
     Network getNetwork() {
@@ -29,31 +28,17 @@ public class TestcontainerConfig {
     @Qualifier("postgres")
     PostgreSQLContainer<?> postgreSQLContainer(Network network) {
         return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
-                .withCreateContainerCmdModifier(cmd -> {
-                    cmd.withName("postgres");
-                    cmd.getHostConfig().withBinds(new Bind("postgres-data", new Volume("/var/lib/postgresql/data")));
-                })
                 .withDatabaseName("test")
                 .withUsername("sa")
                 .withPassword("sa")
                 .withNetwork(network);
     }
 
-    @Bean
-    @RestartScope
-    @Qualifier("pgadmin")
+//    @Bean
+//    @RestartScope
+//    @Qualifier("pgadmin")
     GenericContainer<?> pgAdmin(Network network) {
         var container = new GenericContainer<>(DockerImageName.parse("dpage/pgadmin4"))
-                .withCreateContainerCmdModifier(cmd -> {
-                    cmd.withName("pgadmin-testcontainer");
-                    var hostConfig = cmd.getHostConfig();
-                    var ports = new Ports();
-                    var portBinding = new PortBinding(new Ports.Binding(null, "5050"), ExposedPort.parse("80"));
-                    ports.add(portBinding);
-                    hostConfig.withPortBindings(ports);
-                    hostConfig.withBinds(new Bind("pg-admin", new Volume("/var/lib/pgadmin")));
-                    cmd.withHostConfig(hostConfig);
-                })
                 .withEnv("PGADMIN_DEFAULT_EMAIL", "pgadmin4@abv.bg")
                 .withEnv("PGADMIN_DEFAULT_PASSWORD", "admin")
                 .withNetwork(network);
--- src/test/java/com/tazz/modulith/demo/order/OrderTest.java
+++ src/test/java/com/tazz/modulith/demo/order/OrderTest.java
@@ -1,15 +1,40 @@
 package com.tazz.modulith.demo.order;
 
-import com.tazz.modulith.demo.TestcontainerConfig;
+import com.tazz.modulith.demo.TestTestcontainerConfig;
+import com.tazz.modulith.demo.order.dto.InventoryRequestDto;
+import com.tazz.modulith.demo.order.dto.OrderDto;
+import com.tazz.modulith.demo.order.dto.OrderResponseDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.modulith.test.ApplicationModuleTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@Import(TestcontainerConfig.class)
+import java.util.List;
+
+@Import(TestTestcontainerConfig.class)
+@ActiveProfiles("testcontainers")
 @ApplicationModuleTest(mode = ApplicationModuleTest.BootstrapMode.DIRECT_DEPENDENCIES)
 class  OrderTest {
 
+    @Autowired
+    OrderService orderService;
+
     @Test
     void verifyModule(){
+    }
+
+    @Test
+    void createOrder_shouldPersistDataAndSendEvent() {
+        OrderDto dto = new OrderDto("testName", "test@abv.bg", List.of(
+                new InventoryRequestDto("ruler", 1),
+                new InventoryRequestDto("pencil", 3)
+        ));
+
+        OrderResponseDto responseDto = orderService.createOrder(dto);
+
+        Assertions.assertEquals("Order under process", responseDto.message());
+        Assertions.assertEquals(102, responseDto.statusCode());
     }
 }
